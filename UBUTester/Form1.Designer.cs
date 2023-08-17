b106b383a485e7766dd6681caeea4d27a342b07f
NKolyanich aka Metaler <metaler777@rambler.ru>
2023-08-17 18:43:43 +0300

    one commit

---
+++ UBUTester/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace UBUTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.listViewLog = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBoxSendString = new System.Windows.Forms.TextBox();
+            this.checkBoxLoop = new System.Windows.Forms.CheckBox();
+            this.labelDelay = new System.Windows.Forms.Label();
+            this.maskedTextBoxLoopDelay = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.checkBox_crlf = new System.Windows.Forms.CheckBox();
+            this.checkBox_esc = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(15, 17);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(54, 21);
+            this.comboBoxPort.TabIndex = 0;
+            this.comboBoxPort.SelectedIndexChanged += new System.EventHandler(this.comboBoxPort_SelectedIndexChanged);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(16, 41);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(53, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(320, 15);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(69, 23);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // listViewLog
+            // 
+            this.listViewLog.BackColor = System.Drawing.SystemColors.Window;
+            this.listViewLog.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listViewLog.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listViewLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listViewLog.FullRowSelect = true;
+            this.listViewLog.GridLines = true;
+            this.listViewLog.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewLog.Location = new System.Drawing.Point(0, 113);
+            this.listViewLog.MultiSelect = false;
+            this.listViewLog.Name = "listViewLog";
+            this.listViewLog.ShowGroups = false;
+            this.listViewLog.Size = new System.Drawing.Size(510, 321);
+            this.listViewLog.TabIndex = 4;
+            this.listViewLog.UseCompatibleStateImageBehavior = false;
+            this.listViewLog.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "№";
+            this.columnHeader1.Width = 38;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Время";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Данные";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 303;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "направ.";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 67;
+            // 
+            // serialPort
+            // 
+            this.serialPort.BaudRate = 115200;
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(16, 84);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(77, 23);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textBoxSendString
+            // 
+            this.textBoxSendString.Location = new System.Drawing.Point(11, 17);
+            this.textBoxSendString.MaxLength = 50;
+            this.textBoxSendString.Name = "textBoxSendString";
+            this.textBoxSendString.Size = new System.Drawing.Size(303, 20);
+            this.textBoxSendString.TabIndex = 6;
+            this.textBoxSendString.Text = "ATI";
+            // 
+            // checkBoxLoop
+            // 
+            this.checkBoxLoop.AutoSize = true;
+            this.checkBoxLoop.Location = new System.Drawing.Point(11, 16);
+            this.checkBoxLoop.Name = "checkBoxLoop";
+            this.checkBoxLoop.Size = new System.Drawing.Size(75, 17);
+            this.checkBoxLoop.TabIndex = 9;
+            this.checkBoxLoop.Text = "Включить";
+            this.checkBoxLoop.UseVisualStyleBackColor = true;
+            this.checkBoxLoop.CheckedChanged += new System.EventHandler(this.checkBoxLoop_CheckedChanged);
+            // 
+            // labelDelay
+            // 
+            this.labelDelay.AutoSize = true;
+            this.labelDelay.Location = new System.Drawing.Point(83, 17);
+            this.labelDelay.Name = "labelDelay";
+            this.labelDelay.Size = new System.Drawing.Size(71, 13);
+            this.labelDelay.TabIndex = 11;
+            this.labelDelay.Text = "Период (мс):";
+            // 
+            // maskedTextBoxLoopDelay
+            // 
+            this.maskedTextBoxLoopDelay.Location = new System.Drawing.Point(155, 14);
+            this.maskedTextBoxLoopDelay.Mask = "000000";
+            this.maskedTextBoxLoopDelay.Name = "maskedTextBoxLoopDelay";
+            this.maskedTextBoxLoopDelay.PromptChar = ' ';
+            this.maskedTextBoxLoopDelay.Size = new System.Drawing.Size(59, 20);
+            this.maskedTextBoxLoopDelay.TabIndex = 12;
+            this.maskedTextBoxLoopDelay.Text = "500";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxPort);
+            this.groupBox1.Controls.Add(this.buttonStart);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(85, 71);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Порт";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.maskedTextBoxLoopDelay);
+            this.groupBox2.Controls.Add(this.checkBoxLoop);
+            this.groupBox2.Controls.Add(this.labelDelay);
+            this.groupBox2.Location = new System.Drawing.Point(103, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 42);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Цикл";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBoxSendString);
+            this.groupBox3.Controls.Add(this.buttonSend);
+            this.groupBox3.Location = new System.Drawing.Point(103, 56);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(395, 47);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Данные";
+            // 
+            // checkBox_crlf
+            // 
+            this.checkBox_crlf.AutoSize = true;
+            this.checkBox_crlf.Location = new System.Drawing.Point(337, 23);
+            this.checkBox_crlf.Name = "checkBox_crlf";
+            this.checkBox_crlf.Size = new System.Drawing.Size(59, 17);
+            this.checkBox_crlf.TabIndex = 16;
+            this.checkBox_crlf.Text = "CR+LF";
+            this.checkBox_crlf.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_esc
+            // 
+            this.checkBox_esc.AutoSize = true;
+            this.checkBox_esc.Location = new System.Drawing.Point(402, 23);
+            this.checkBox_esc.Name = "checkBox_esc";
+            this.checkBox_esc.Size = new System.Drawing.Size(47, 17);
+            this.checkBox_esc.TabIndex = 17;
+            this.checkBox_esc.Text = "ESC";
+            this.checkBox_esc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 434);
+            this.Controls.Add(this.checkBox_esc);
+            this.Controls.Add(this.checkBox_crlf);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.listViewLog);
+            this.Name = "Form1";
+            this.Text = "ATTerminal";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.ListView listViewLog;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBoxSendString;
+        private System.Windows.Forms.CheckBox checkBoxLoop;
+        private System.Windows.Forms.Label labelDelay;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxLoopDelay;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.CheckBox checkBox_crlf;
+        private System.Windows.Forms.CheckBox checkBox_esc;
+    }
+}
+
---
+++ UBUTester/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace UBUTester
+{
+    public enum Command
+    {
+        COMMAND_
+    };
+
+    public enum ANSWER_CODE
+    {
+        OK = 0x00,
+        CONNECT,
+        RING,
+        NO_CARRIER,
+        ERROR,
+        NO_DIALTONE,
+        BUSY,
+        NO_ANSWER,
+        NOT_SUPPORT,
+        INVALID_COMMAND_LINE
+    };
+
+    public struct MSGRead
+    {
+
+        public string AnswerCommand;
+        public int AnswerCode;
+        public int SizeData;
+        public byte[] Data;
+    };
+
+    public partial class Form1 : Form
+    {
+        Timer loop;
+        int AnswerFlag = 0; // 0 - ожидаем ответной команды. 1 - ожидаем кода ответа. 2 - ожидаем параметров ответа
+        const int BUF_LEIGHT = 255;
+        byte[] readBuf = new byte[BUF_LEIGHT];
+        MSGRead[] mgread = new MSGRead[10];
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBoxPort.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+            serialPort.BaudRate = 115200;
+            checkBoxLoop.Checked = false;
+            labelDelay.Enabled = false;
+            maskedTextBoxLoopDelay.Enabled = false;
+            OnReceive += new MethodReceive(SerialPort_OnReceive);
+            loop = new Timer();
+            loop.Tick += new EventHandler(loop_Tick);
+            DateTime dt = DateTime.Now;
+            WriteStringInLog("\r\n//------ "+dt.ToLongDateString()+" "+dt.ToLongTimeString()+"---------------- //");
+
+            toolTip.SetToolTip(comboBoxPort, "Выбор COM-порта.");
+            toolTip.SetToolTip(buttonStart, "Запуск/Останов выбранного COM-порта.");
+            toolTip.SetToolTip(checkBoxLoop, "Включение/выключение цикличной посылки сообщения.");
+            toolTip.SetToolTip(maskedTextBoxLoopDelay, "Период посылки сообщения, в миллисекундах.");
+            toolTip.SetToolTip(buttonClear, "Очистка списка. Лог НЕ стирается.");
+            toolTip.SetToolTip(textBoxSendString, "Сообщение в hex.\nС пробелами или без.\nМаксимум 50 символов. \nРасчёт CRC - автоматически, для Modbus-RTU");
+            toolTip.SetToolTip(buttonSend, "1)Отправка сообщения\n2)Запуск/останов цикла посылки сообщения.");
+        }
+
+        void loop_Tick(object sender, EventArgs e)
+        {
+            SendHexString(textBoxSendString.Text);
+        }
+
+        public delegate void MethodReceive(byte[] str);
+        public event MethodReceive OnReceive;
+
+
+        void SerialPort_OnReceive(byte[] data)
+        {
+            SetReceiveTerminalTextChange(data);
+        }
+
+        delegate void SetReceiveTextCallback(byte[] data);
+        void SetReceiveTerminalTextChange(byte[] data)
+        {
+            if (this.listViewLog.InvokeRequired)
+            {
+                SetReceiveTextCallback dt = new SetReceiveTextCallback(SetReceiveTerminalTextChange);
+                this.Invoke(dt, new object[] { data });
+            }
+            else
+            {
+                string[] str = new string[listViewLog.Columns.Count];
+                DateTime dt = DateTime.Now;
+                string strdata = "";
+                for (int i = 0; i < data.Length; i++)
+                {
+                    if (Convert.ToChar(data[i]) == 0x0d)
+                    {
+                        if ( AnswerFlag == 0 )
+                        {// если только получили ответ
+
+                        }
+                        //strdata += Convert.ToString(data[i],);
+                        strdata += Convert.ToChar(data[i]).ToString();
+                    }
+                    else
+                    {
+                        strdata += Convert.ToChar(data[i]).ToString();
+                        continue;
+                    }
+                }
+                str[0] = (listViewLog.Items.Count + 1).ToString();
+                str[1] = dt.ToLongTimeString() + "." + dt.Millisecond.ToString();
+                str[2] = strdata;
+                str[3] = " <<-- ";
+                listViewLog.Items.Add(new ListViewItem(str));
+                DownScroll();
+                WriteStringInLog(String.Join("    ", str));
+
+                listViewLog.Items[listViewLog.Items.Count - 1].BackColor = Color.Salmon;
+            }
+        }
+
+        private void comboBoxPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort.PortName = (string)comboBoxPort.SelectedItem;
+        }
+
+
+
+        int _stepIndex = 0;
+        bool _startRead = false;
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        { //принимаем пакет
+            try
+            { 
+                //  узнаем сколько байт пришло
+                int buferSize = serialPort.BytesToRead;
+                for (int i = 0; i < buferSize; ++i)
+                {
+                    //  читаем по одному байту
+                    byte bt = (byte)serialPort.ReadByte();
+                    //  если встретили начало кадра (0xFF) - начинаем запись в _bufer
+                    if (0x0A == bt)
+                    {
+                        bt = (byte)serialPort.ReadByte();
+                        if ( 0x0D == bt )
+                        {
+                            if (_startRead)
+                            {
+                                //  когда буфер наполнлся данными
+                                _startRead = false;
+                                byte[] bu = new byte[_stepIndex];
+                                for (int y = 0; y < _stepIndex; y++)
+                                {
+                                    bu[y] = readBuf[y];
+                                }
+                                //- разбираем что приняли----------------
+                                OnReceive(bu);
+                            }
+                            else
+                            {
+                                _stepIndex = 0;
+                                _startRead = true;
+                                //goto exit;
+                            }
+                        }
+                    }
+                    //  дописываем в буфер все остальное
+                    if (_startRead)
+                    {
+                        readBuf[_stepIndex] = bt;
+                        ++_stepIndex;
+                    }
+                    //exit: ;
+                }
+            }
+            catch { }
+
+            //////////////
+            //serialPort.Read(readBuf, 0, BUF_LEIGHT);
+            //OnReceive(readBuf);
+        }
+
+        #region Button Start
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                serialPort.Open();
+                buttonStart.Text = "Стоп";
+                comboBoxPort.Enabled = false;
+            }
+            else
+            {
+                serialPort.Close();
+                buttonStart.Text = "Старт";
+                comboBoxPort.Enabled = true;
+            }
+        }
+        #endregion
+
+        #region Button Send
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                if (!checkBoxLoop.Checked)
+                {
+                    SendHexString(textBoxSendString.Text);
+                }
+                else
+                {
+                    if (loop.Enabled)
+                    {
+                        buttonSend.Text = "Запуск";
+                        loop.Stop();
+                    }
+                    else
+                    {
+                        buttonSend.Text = "Стоп";
+                        loop.Interval = Convert.ToInt32(maskedTextBoxLoopDelay.Text);
+                        loop.Start();
+                    }
+                }     
+            }
+            else
+            {
+                MessageBox.Show("Порт: \"" + serialPort.PortName + "\" НЕ открыт!");
+            }
+        }
+
+        private void SendHexString(string Data)
+        {
+            try
+            {
+                if (checkBox_crlf.Checked == true)
+                {
+                    Data = Data + "\r\n";
+                }
+                if (checkBox_esc.Checked)
+                {
+                    Data = Data + Convert.ToString(0x1B);
+                }
+                serialPort.Write(Data);
+
+                string[] str = new string[listViewLog.Columns.Count];
+                DateTime dt = DateTime.Now;
+                str[0] = (listViewLog.Items.Count + 1).ToString();
+                str[1] = dt.ToLongTimeString() + "." + dt.Millisecond.ToString();
+                str[2] = Data;
+                str[3] = " -->> ";
+
+                listViewLog.Items.Add(new ListViewItem(str));
+                DownScroll();
+                listViewLog.Items[listViewLog.Items.Count - 1].BackColor = Color.LightBlue;
+
+                WriteStringInLog(String.Join("    ", str));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        #endregion
+
+        #region Button Clear
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            listViewLog.Items.Clear();
+        }
+        #endregion
+
+        private void DownScroll()
+        {
+            listViewLog.EnsureVisible(listViewLog.Items.Count - 1);
+        }
+
+        #region //ModRTU_CRC
+        byte[] ModRTU_CRC(byte[] buf, int len)
+        {
+            UInt16 crc = 0xFFFF;
+
+            for (int pos = 0; pos < len; pos++)
+            {
+                crc ^= (UInt16)buf[pos];
+
+                for (int i = 8; i != 0; i--)
+                {
+                    if ((crc & 0x0001) != 0)
+                    {
+                        crc >>= 1;
+                        crc ^= 0xA001;
+                    }
+                    else
+                        crc >>= 1;
+                }
+            }
+            //  младший и старший байты поменяны местами
+            byte[] crc16 = new byte[2];
+            crc16[1] = (byte)((crc/256) & 0x00ff);	 //  Hi byte
+            crc16[0] = (byte)(crc & 0x00ff); 		 //  Lo byte
+            return crc16;
+        }
+        #endregion
+
+        private void WriteStringInLog(string data)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(Application.StartupPath + @"\log.log", true))
+            {
+                file.WriteLine(data);
+            }
+        }
+
+        private void checkBoxLoop_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxLoop.Checked)
+            {
+                labelDelay.Enabled = true;
+                maskedTextBoxLoopDelay.Enabled = true;
+                buttonSend.Text = "Запуск";
+            }
+            else
+            {
+                labelDelay.Enabled = false;
+                maskedTextBoxLoopDelay.Enabled = false;
+                buttonSend.Text = "Отправить";
+            }
+        }
+
+    }
+}
